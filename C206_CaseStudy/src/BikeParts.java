978c4998a8e45d5d8ea5b329adfde29f856b16b9
19029532 <19029532@DESKTOP-LQAV2GH>
2020-08-18 10:20:45 +0800

    -

---
+++ C206_CaseStudy/src/BikeParts.java
@@ -0,0 +1,47 @@
+
+public class BikeParts {
+
+	//declare class parameters
+	private String partName;
+	private String description;
+	private boolean isAvailable;
+	
+	
+	public BikeParts(String model, String description, boolean isAvailable) {
+		//initialize paramaters
+	}
+	
+	public String getPartName() {
+		return partName;
+	}
+
+
+	public void setPartName(String partName) {
+		this.partName = partName;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	//public String toString() {
+		//format string to return
+				
+	//}
+}
---
+++ C206_CaseStudy/src/BikePartsDB.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+
+public class BikePartsDB {
+	
+//	private static ArrayList<Bike> bikeList = new ArrayList<Bike>();
+	
+	/*public static void addBike(Bike) {
+		//add some data to the list
+		
+	}
+	
+	public static void delBike(Bike) {
+		//clean up the data in the list
+		
+	}
+	
+	*/
+	public static void viewAllBikeParts() {
+		//show bike menu, ask user for input, process the choice
+		int subOption = 0;
+		
+		
+	}
+	
+	public static void showBikePartsMenu() {
+		//Display the menu
+	}	
+	
+	public static void processOption(int subOption) {
+			
+		while (subOption != 4) {
+			if (subOption == 1) {
+				// View all bike
+			
+				
+		
+			} else if (subOption  == 2) {
+				// Add a bike
+				
+		
+			} else if (subOption  == 3) {
+				// Delete a bike
+				
+							
+			}else {
+				//invalid option
+				System.out.println("Invalid type");
+			}
+			
+			//show the menu again & ask for option
+			
+			
+		}
+	
+	}
+	
+	//==========Option 1 ==============
+	public static void viewAllBike() {
+		//display all bikes in the list
+	}
+	
+	public static String retrieveAllBikeParts() {
+		return null;
+		//retrieve and format all bikes in the list
+	}
+	
+	
+	//==========Option 2 ==============
+	public static void inputBikeDetails() {
+		//request user for the bike to add to the list
+	}
+	
+	public static void addBike(BikeParts bk) {
+		//add bike to list
+		
+	}
+	
+		
+	//==========Option 3 ==============
+/*	public static int selectBike() {
+		//request the user to select bike for process
+	}
+	
+	public static void delBike(int bikeID) {
+		//remove bike to list
+		
+	}
+	
+	
+	//==============Helper Functions=======
+	public static int getBikeDBSize() {
+		return bikeList.size();
+	}
+	
+	public static String getBikeInfo(int bikeID) {
+		//format and return the string
+	}*/
+}
